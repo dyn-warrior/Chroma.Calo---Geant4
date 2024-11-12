adbaaf51b30a98de10446fe30dd8231164e79f6e
RATUL TARAFDER <166398989+dyn-warrior@users.noreply.github.com>
2024-11-12 22:39:47 +0530

    Update main.cc

--- main.cc
+++ main.cc
@@ -10,18 +10,25 @@
 #include "FTFP_BERT.hh" // Include a predefined physics list
 
 int main(int argc, char** argv) {
+/* adddition 000 Detect interactive mode (if no arguments) and define UI session
+    G4UIExecutive* ui = nullptr;
+    if (argc == 1) {
+        ui = new G4UIExecutive(argc, argv);
+    }*/
     G4RunManager* runManager = new G4RunManager();
 
     // Set mandatory initialization classes
     runManager->SetUserInitialization(new MyDetectorConstruction());
     runManager->SetUserInitialization(new FTFP_BERT()); // Set the physics list
+    // runManager->SetUserInitialization(new G4VisExecutive());
+
 
     // Set user action classes
     runManager->SetUserAction(new MyPrimaryGeneratorAction());
     runManager->SetUserAction(new MyRunAction());
 
     G4SDManager* sdManager = G4SDManager::GetSDMpointer();
-    MySensitiveDetector* sensitiveDetector = new MySensitiveDetector("SensitiveDetector");
+    MySensitiveDetector* sensitiveDetector = new MySensitiveDetector("SensitiveDetector", "HitsCollection");
     sdManager->AddNewDetector(sensitiveDetector);
 
     runManager->Initialize();
@@ -41,6 +48,13 @@ int main(int argc, char** argv) {
         G4String fileName = argv[1];
         uiManager->ApplyCommand(command + fileName);
     }
+    
+  /*   // Perform multiple runs
+    G4int numberOfRuns = 10;  // Number of runs
+    for (G4int i = 0; i < numberOfRuns; ++i) {
+        runManager->BeamOn(1);  // Run 1 event per run
+    }*/
+
 
     delete visManager;
     delete runManager;
